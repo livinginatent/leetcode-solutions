d2814278359d7692a6860dde9406444f341207fc
livinginatent <suleyman.eminbeyli@gmail.com>
2024-02-06 16:22:59 +0400

    isValidParentheses

--- is-valid-parentheses.js
+++ is-valid-parentheses.js
@@ -1,17 +1,14 @@
 const isValidParentheses = (str) => {
-  const stack = []
-  const pairs = { "(": ")", "{": "}", "[": "]" };
-  for (const char of str){
-    if(pairs[char]){
-        stack.push(pairs[char])
-    }else{
-        if(stack.pop()!==char){
-            return false
-        }
+  const stack = [];
+  const pairs = { "(": ")", "[": "]", "{": "}" };
+  for (char in str) {
+    if (pairs[char]) {
+      stack.push(pairs[char]);
+    } else {
+      if (stack.pop() !== char) {
+        return false;
+      }
     }
   }
-  console.log(stack.length===0)
-  return stack.length===0
-}
-
-isValidParentheses('()')
+  return stack.length === 0;
+};
