e7941558406106502536e64e81dcdfd1c0917f0f
livinginatent <suleyman.eminbeyli@gmail.com>
2024-02-09 10:51:40 +0400

    refactor to mergeStringsAlternately

---
+++ increasing-triplet-sequence.js
@@ -0,0 +1,14 @@
+const increasingTriplet = (nums) => {
+  const stack = [];
+  let i =0
+  let j= 1
+  let k = 2
+  
+  for (_; i < nums.length; i++) {
+   
+  }
+};
+
+increasingTriplet([10,4,12,8,15,16,1,17,]);
+
+//
--- merge-strings-alternately.js
+++ merge-strings-alternately.js
@@ -1,15 +1,14 @@
-const mergeAlternately = (input1, input2) => {
-  const word1 = input1.split("");
-  const word2 = input2.split("");
-  const length = Math.max(word1.length, word2.length);
-  const mergedArray = [];
-  for (let i = 0; i < length; i++) {
-    if (word1[i]) {
-      mergedArray.push(word1[i]);
+const mergeAlternately = (word1,word2) => {
+  const len = Math.max(word1.length,word2.length)
+  let result = ''
+  for(let i=0;i<len;i++){
+    if(word1[i]){
+      result+=word1[i]
     }
     if (word2[i]) {
-      mergedArray.push(word2[i]);
+      result += word2[i];
     }
   }
-  return mergedArray.join("");
-};
+  return result
+}
+
